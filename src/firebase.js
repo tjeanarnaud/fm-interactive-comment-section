4be1c9da57b50a5ef82a803410a35533ac49794c
tjeanarnaud <68494067+tjeanarnaud@users.noreply.github.com>
2022-06-28 01:32:58 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
   messagingSenderId: '947470322318',
-  appId: process.env.FIREBASE_APP_ID,
+  appId: `${process.env.FIREBASE_APP_ID}`,
   measurementId: 'G-T7Z9S5XDB4',
 }
 
